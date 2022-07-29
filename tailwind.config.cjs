72f6f7c567d180281d3a65c13f8b1bae072e1ec3
ihzavip <39728148+ihzavip@users.noreply.github.com>
2022-07-29 23:57:24 +0700

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {},
+		colors: {
+			'green-main': '#244D4D'
+		}
+	},
+	plugins: []
+};
