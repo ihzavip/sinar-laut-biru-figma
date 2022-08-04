c231be8559c0c5e2b691c4b3fbc432d986a72d88
ihzavip <39728148+ihzavip@users.noreply.github.com>
2022-08-04 21:09:11 +0700

    Add files via upload
    implemented key feature.
    need to fix :
    1.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,86 +1,197 @@
 <!DOCTYPE html />
 <html lang="en">
-	<head>
-		<meta charset="UTF-8" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<title>PT. Sinar Laut Biru</title>
-	</head>
-	<body>
-		<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
-			<div class="container flex flex-wrap justify-between items-center mx-auto">
-				<a href="https://flowbite.com/" class="flex items-center">
-					<span class="self-center text-xl font-semibold whitespace-nowrap text-green-main dark:text-white"
-						>PT. Sinar <br> Laut Biru</span
-					>
-				</a>
-				<button
-					data-collapse-toggle="navbar-default"
-					type="button"
-					class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-					aria-controls="navbar-default"
-					aria-expanded="false"
-				>
-					<span class="sr-only">Open main menu</span>
-					<svg
-						class="w-6 h-6"
-						aria-hidden="true"
-						fill="currentColor"
-						viewBox="0 0 20 20"
-						xmlns="http://www.w3.org/2000/svg"
-						><path
-							fill-rule="evenodd"
-							d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-							clip-rule="evenodd"
-						/></svg
-					>
-				</button>
-				<div class="hidden w-full md:block md:w-auto" id="navbar-default">
-					<ul
-						class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
-					>
-						<li>
-							<a
-								href="#home"
-								class="block py-2 pr-4 pl-3 text-green-main bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-								aria-current="page">Home</a
-							>
-						</li>
-						<li>
-							<a
-								href="#home"
-								class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-								>About</a
-							>
-						</li>
-						<li>
-							<a
-								href="#home"
-								class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-								>Services</a
-							>
-						</li>
-						<li>
-							<a
-								href="#home"
-								class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-								>Pricing</a
-							>
-						</li>
-						<li>
-							<a
-								href="#home"
-								class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-								>Contact</a
-							>
-						</li>
-					</ul>
-				</div>
-				<div class="hidden  md:inline-flex gap-5">
-					<img src="./icon/search.png" alt="" />
-					<img src="./icon/menu.png" alt="" />
-				</div>
-			</div>
-		</nav>
-	</body>
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>PT. Sinar Laut Biru</title>
+  </head>
+  <body>
+    <nav
+      class="container mx-auto px-5 flex justify-between items-center py-2 md:px-24 "
+    >
+      <!-- LOGO -->
+      <a href="#home" class="text-xl  md:text-2xl text-main-green font-bold"
+        >PT. Sinar <br /> Laut Biru</a
+      >
+
+      <ul class="hidden md:flex gap-10 ">
+        <li><a href="#a">Home</a></li>
+        <li><a href="#a">About Us</a></li>
+        <li><a href="#a">Features</a></li>
+        <li><a href="#a">Product</a></li>
+        <li><a href="#a">Contact</a></li>
+      </ul>
+
+      <div class="icon flex gap-3 ">
+        <a href="#search"><img src="icon/search.png" alt="" /></a>
+        <a href="#search"><img src="icon/menu.png" alt="" /></a>
+      </div>
+    </nav>
+
+    <!-- Main -->
+    <main class="container mx-auto  md:px-5 md:relative ">
+      <div
+        class="md:w-[42%] main-text bg-main-green px-5 py-10 md:py-16 md:px-12 text-white md:relative md:p-10 md:top-16 md:left-28 "
+      >
+        <h2 class="text-3xl font-bold md:text-5xl tracking-wider ">
+          We Make it Easy <br /> to Shop Between <br /> Countries
+        </h2>
+        <p class="md:text-xl md:mt-8 md:mb-12 md:leading-6 my-6  ">
+          We will help you to transacting <br /> between countries safely and
+          <br /> comfortably.
+        </p>
+
+        <a
+          href="#button"
+          class="px-6 py-3 text-sm text-main-green bg-white font-semibold rounded-full border border-white hover:text-white hover:bg-main-green hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+          >Explore More</a
+        >
+      </div>
+
+      <img
+        src="media/hero-1-edited.png"
+        alt=""
+        class="md:absolute relative md:top-10 md:left-52 md:-z-10"
+      />
+    </main>
+
+    <div class="container mx-auto md:static md:mt-48 mt-8 ">
+      <h3 class="text-main-green md:text-2xl text-center ">We are Joined in</h3>
+
+      <div
+        class="affiliation flex justify-center items-center md:gap-16 md:my-12"
+      >
+        <img src="media/go4worldbussiness.png" alt="" />
+        <img src="media/visa.png" alt="" />
+        <img src="media/paypal.png" alt="" />
+        <img src="media/mastercard.png" alt="" />
+      </div>
+    </div>
+
+    <!-- ABOUT US -->
+    <div class=" container mx-auto md:px-24 md:flex justify-center md:gap-10">
+      <div class="about-text md:w-[50%] md:mr-10">
+        <h3 class="md:text-3xl text-main-green md:pb-5">About Us</h3>
+        <p class="text-main-green text-lg">
+          Sinar Laut Biru is an EXIM Company that located at Gunung Kidul,
+          Yogyakarta. Medan, North Sumatera. We trying to be the best partner in
+          providing Agricultural product like Coconut, Cabbage, Carrot and etc.
+          Basically we are a company enganged in furniture products and wood
+          shaving, over time we started to help our customers to meet their
+          needs for agricultural products. Total customer satisfaction is the
+          main focus of our company.
+        </p>
+      </div>
+
+      <div class="about-img">
+        <img class="" src="media/about-img.png" alt="" />
+      </div>
+    </div>
+
+    <!-- OUR KEY FEATURE -->
+
+    <h3 class="md:text-3xl text-main-green md:pb-5 md:my-10 text-center">
+      Our Key Feature
+    </h3>
+    <div
+      class="key-features container md:rounded-2xl mx-auto md:px-24 md:text-center md:flex md:justify-center md:items-center md:gap-3"
+    >
+      <div class="max-w-xs rounded overflow-hidden shadow-lg bg-main-green">
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2 text-white">
+            The Coldest Sunset
+          </div>
+          <p class="text-white text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+      </div>
+
+      <div class="max-w-xs rounded overflow-hidden shadow-lg bg-main-green">
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2 text-white">
+            The Coldest Sunset
+          </div>
+          <p class="text-white text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+      </div>
+
+      <div class="max-w-xs rounded overflow-hidden shadow-lg bg-main-green">
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2 text-white">
+            The Coldest Sunset
+          </div>
+          <p class="text-white text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+      </div>
+
+      <div class="max-w-xs rounded overflow-hidden shadow-lg bg-main-green">
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2 text-white">
+            The Coldest Sunset
+          </div>
+          <p class="text-white text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Product -->
+    <section class="product container mx-auto md:px-24 md:mt-10">
+      <div class="product-text w-[60%] text-main-green">
+        <h3 class="md:text-3xl ">Our Popular Product</h3>
+        <div class="bg-red-400 flex-product-arrow md:flex justify-center items-center">
+          <p>
+            All of our furniture uses the best materials and choices for our
+            customers.All of our furniture uses the best materials and choices
+            for our customers.
+          </p>
+          <div class="arrow-icon md:flex md:gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M10 19l-7-7m0 0l7-7m-7 7h18"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M14 5l7 7m0 0l-7 7m7-7H3"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </section>
+  </body>
 </html>
